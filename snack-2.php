06d7fb9f504769d190fc7a8dd50df1ae3803e0d8
Giulio_Pa <giuliop93@hotmail.it>
2021-06-01 10:41:38 +0200

    o.O

--- snack-2.php
+++ snack-2.php
@@ -1,11 +1,43 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 <?php
-   $name = $_GET["name"];
-   $mail = $_GET["mail"];
-   $age = $_GET["age"];
+
+$name = "";
+$mail = "";
+$age = "";
+
+if(key_exists("name", $_GET)){
+   $name = $_GET["name"]
+}
+
+if(key_exists("name", $_GET)){
+   $name = $_GET["mail"]
+}
+
+if(key_exists("name", $_GET)){
+   $name = $_GET["age"]
+}
+
+$result = true;
+
+if(strlen($name) < 3) {
+   $result = false;
+}
+
+if(!strpos($mail, "@")) {
+   $result = false;
+}
+
+if(strpos($mail, ".", strpos($mail, "@"))){
+   $result = false;
+}
+
+if(!is_int($age)) {
+   $result = false;
+}
+
    $res = "Accesso negato";
    if ( strlen($name) > 2 && strpos($mail, ".") && strpos($mail, "@") && filter_var($age, FILTER_VALIDATE_INT)) {
-      $res = "Accesso riuscito";
+      $result = "Accesso riuscito";
    }
 ?>
 
--- snack-4.php
+++ snack-4.php
@@ -3,4 +3,34 @@ lo stesso numero più di una volta
 
 <?php
 
+$maxNUmbers = 15;
+$numbersList = [];
 
+for ($i = i; $i <= 15; $i++) {
+    $randomNumber = rand(1, 100);
+
+    if(!in_array($randomNumber, $numbersList)){
+        $numbersList[] = randomNumbers;
+    } else {
+        $i--;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    <?php
+    for($i = 1; $i < count($numbersList)
+    </p>
+
+</body>
+</html>
